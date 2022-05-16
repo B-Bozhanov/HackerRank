640334ced03ebe77acc131f58d164553143c690a
DareBDeviL <bojanbojanovsot@gmail.com>
2022-05-16 16:12:43 +0300

    Add rotation

--- Algorithms/MatrinxRotation/Program.cs
+++ Algorithms/MatrinxRotation/Program.cs
@@ -8,30 +8,23 @@ namespace MatrinxRotation
     {
         static void Main(string[] args)
         {
-            //var matrixInfo = Console.ReadLine().Trim().Split(' ');
-            //var rows = int.Parse(matrixInfo[0]);
-            //var cols = int.Parse(matrixInfo[1]);
-            //var rotations = int.Parse(matrixInfo[2]);
+            var matrixInfo = Console.ReadLine().Trim().Split(' ');
+            var rows = int.Parse(matrixInfo[0]);
+            var cols = int.Parse(matrixInfo[1]);
+            var rotations = int.Parse(matrixInfo[2]);
 
-            //var matrix = new List<List<int>>();
+            var matrix = new List<List<int>>();
 
-            //for (int row = 0; row < rows; row++)
-            //{
-            //    matrix.Add(Console.ReadLine().
-            //        Trim().
-            //        Split(' ').Select(x => int.Parse(x)).
-            //        ToList());
-            //}
-
-            var matrix = new List<List<int>>()
+            for (int row = 0; row < rows; row++)
             {
-                 new List<int>{ 1,  2,  3,  4 },
-                 new List<int>{ 5,  6,  7,  8 },
-                 new List<int>{ 9, 10, 11, 12 },
-                 new List<int>{13, 14, 15, 16 }
-            };
+                matrix.Add(Console.ReadLine().
+                    Trim().
+                    Split(' ').Select(x =>int.Parse(x)).
+                    ToList());
+            }
 
-            Result.MatrixRotation(matrix, 1);
+
+            Result.MatrixRotation(matrix, rotations);
         }
     }
 }
