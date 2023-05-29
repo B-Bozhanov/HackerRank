1cbbe6a8e2cb1bce5b9123944c70427baf26e34a
Bozhan Bozhanov <bojanbojanovsot@gmail.com>
2023-05-29 09:31:23 +0300

    Update Program.cs

--- Algorithms/MatrinxRotation/Program.cs
+++ Algorithms/MatrinxRotation/Program.cs
@@ -12,6 +12,7 @@ namespace MatrinxRotation
             var rows = int.Parse(matrixInfo[0]);
             var cols = int.Parse(matrixInfo[1]);
             var rotations = int.Parse(matrixInfo[2]);
+            var test = 5;
 
             var matrix = new List<List<int>>();
 
